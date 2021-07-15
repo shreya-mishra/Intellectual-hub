5b94b1a159987fc44e0ba0068532fb58cd7aa8de
SHREYA MISHRA <61110378+shreya-mishra@users.noreply.github.com>
2021-07-15 18:46:53 +0530

    added production mode

--- backend/Server.js
+++ backend/Server.js
@@ -1,25 +1,40 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import colors from "colors";
-import { notFound, errorHandler } from './middleware/errorMiddlewares.js';
+import express from "express";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddlewares.js";
 import dotenv from "dotenv";
-
-
+import path from "path";
 dotenv.config();
 connectDB();
 
 const app = express();
 
 // GET Request to "/" route
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api", (req, res) => {
-    res.json("API is running..");
+  res.json("API is running..");
 });
 app.use("/api/user", userRoutes);
+// -----------------------DEPLOYMENT----------------------------
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "intellectual-hub/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "intellectual-hub", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+// -----------------------DEPLOYMENT----------------------------
 app.use(notFound);
 app.use(errorHandler);
 
-
-app.listen(process.env.PORT, console.log(`Server running on PORT ${process.env.PORT}..`.bold));
+app.listen(
+  process.env.PORT,
+  console.log(`Server running on PORT ${process.env.PORT}..`.bold)
+);
